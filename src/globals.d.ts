fc9a8c5ca3d60fd81a2f6b0afe055a6847b2bcf9
Tisitha <tisitha007@gmail.com>
2025-10-08 19:02:25 +0530

    refactor(type)

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,65 @@
 declare module '*.css';
+
+declare global {
+    type productResponseType = {
+        productResponseDtoList: product[];
+        totalElement: 0;
+        pageCount: 0;
+        isLast: true;
+      };
+      
+      type productType = {
+        id: string;
+      
+        vendorProfile: any;
+      
+        name: string;
+      
+        imgUrl: string;
+      
+        description: string;
+      
+        price: number;
+      
+        deal: number;
+      
+        cod: boolean;
+      
+        freeDelivery: boolean;
+      
+        brand: string;
+      
+        category: any;
+      
+        reviews: any;
+      
+        avgRatings: number;
+      
+        province: any;
+      
+        warranty: any;
+      
+        questions: any;
+      
+        cartItems: any;
+      
+        quantity: number;
+      };
+      
+      type categoryType = {
+        id: number;
+        name: string;
+      };
+      
+      type provinceType = {
+        id: number;
+        name: string;
+      };
+      
+      type warrantyType = {
+        id: number;
+        name: string;
+      };
+  }
+  
+  export {};
