3a6006ad9809c6bd5c05642c0debc6e1ef32033b
Tisitha <tisitha007@gmail.com>
2025-10-10 22:32:43 +0530

    feat(global types): update types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,65 +1,107 @@
-declare module '*.css';
-
 declare global {
-    type productResponseType = {
-        productResponseDtoList: product[];
-        totalElement: 0;
-        pageCount: 0;
-        isLast: true;
-      };
-      
-      type productType = {
-        id: string;
-      
-        vendorProfile: any;
-      
-        name: string;
-      
-        imgUrl: string;
-      
-        description: string;
-      
-        price: number;
-      
-        deal: number;
-      
-        cod: boolean;
-      
-        freeDelivery: boolean;
-      
-        brand: string;
-      
-        category: any;
-      
-        reviews: any;
-      
-        avgRatings: number;
-      
-        province: any;
-      
-        warranty: any;
-      
-        questions: any;
-      
-        cartItems: any;
-      
-        quantity: number;
-      };
-      
-      type categoryType = {
-        id: number;
-        name: string;
-      };
-      
-      type provinceType = {
-        id: number;
-        name: string;
-      };
-      
-      type warrantyType = {
-        id: number;
-        name: string;
-      };
-  }
-  
-  export {};
+  type productResponseType = {
+    productResponseDtoList: productType[];
+    totalElement: 0;
+    pageCount: 0;
+    isLast: boolean;
+  };
+
+  type questionResponseType = {
+    questionResponseDtoList: questionType[];
+    totalElement: 0;
+    pageCount: 0;
+    isLast: boolean;
+  };
+
+  type reviewResponseType = {
+    reviewResponseDtoList: reviewType[];
+    totalElement: 0;
+    pageCount: 0;
+    isLast: boolean;
+  };
+
+  type productType = {
+    id: string;
+
+    vendorProfile: vendorProfileType;
+
+    name: string;
+
+    imgUrl: string;
+
+    description: string;
+
+    price: number;
+
+    deal: number;
+
+    cod: boolean;
+
+    freeDelivery: boolean;
+
+    brand: string;
+
+    category: categoryType;
+
+    avgRatings: number;
+
+    province: provinceType;
+
+    warranty: warrantyType;
+
+    quantity: number;
+  };
+
+  type categoryType = {
+    id: number;
+    name: string;
+  };
+
+  type provinceType = {
+    id: number;
+    name: string;
+  };
+
+  type warrantyType = {
+    id: number;
+    name: string;
+  };
+
+  type vendorProfileType = {
+    id: string;
+    businessName: string;
+  };
+
+  type reviewType = {
+    id: number;
+    body: string;
+    rate: number;
+    date: string;
+    user: userType;
+    edited: boolean;
+    product: productType;
+  };
+
+  type questionType = {
+    id: number;
+    question: string;
+    answer: string;
+    user: userType;
+    date: string;
+    product: string;
+  };
+
+  type userType = {
+    id: string;
+    fname: string;
+    lname: string;
+    email: string;
+  };
+
+  type option = {
+    name: string;
+    href: string;
+  };
+}
+
+export {};
