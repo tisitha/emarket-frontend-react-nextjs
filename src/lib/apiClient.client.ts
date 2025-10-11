9d0421415b18399214f0a1f72ddc843cef9326ec
Tisitha <tisitha007@gmail.com>
2025-10-11 18:24:36 +0530

    refactor(apiClient)

--- src/lib/apiClient.client.ts
+++ src/lib/apiClient.client.ts
@@ -1,7 +1,9 @@
+import { toast } from "sonner";
+
 export async function apiFetchClient<T>(
   endpoint: string,
   options?: RequestInit
-): Promise<T | null> {
+): Promise<T | null | true> {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
       ...options,
@@ -12,12 +14,26 @@ export async function apiFetchClient<T>(
       credentials: "include",
     });
 
-    if (!res.ok) {
+    if (res.ok) {
+      try {
+        const output = (await res.json()) as T;
+        return output;
+      } catch {
+        return true;
+      }
+    } else {
       console.error("[apiFetch/client] HTTP error:", res.status);
+      const errdata: errdataType = await res.json();
+      if (errdata.detail) {
+        toast.error(errdata.detail);
+      }
+      if (errdata.errors) {
+        errdata.errors.forEach((item) => {
+          toast.error(item.field + " - " + item.message);
+        });
+      }
       return null;
     }
-
-    return (await res.json()) as T;
   } catch (err) {
     console.error("[apiFetch/client]", err);
     return null;
