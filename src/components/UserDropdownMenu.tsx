3efdf33d1c2cefe8cd7997a0937a8debea61fcee
Tisitha <tisitha007@gmail.com>
2025-10-18 00:15:11 +0530

    fix(userDropDownMenu)

--- src/components/UserDropdownMenu.tsx
+++ src/components/UserDropdownMenu.tsx
@@ -7,7 +7,10 @@ const UserDropdownMenu = ({ name }: { name: string }) => {
       <div tabIndex={0} className="btn leading-none rounded-3xl">
         Hello {name}
       </div>
-      <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <ul
+        tabIndex={0}
+        className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+      >
         <li>
           <Link href={"/dashboard"}>Dashboard</Link>
         </li>
